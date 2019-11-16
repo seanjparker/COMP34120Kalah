e4d7da956419de41352f0a9b78092cd2d86d524b
Sara Caballero Bruno <sara2cb@gmail.com>
2019-11-16 17:43:51 +0000

    Created a getChildren function for the MiniMax method

--- MiniMax.java
+++ MiniMax.java
@@ -1,28 +1,41 @@
+import java.util.ArrayList;
+
+import MKAgent.Board;
 
 public class MiniMax {
-	public int minimax(State state, int depth, boolean maximizingPlayer) {
-		if(depth==0 || state.children()==null) {
-			return valueFunction(state);
+	public int minimax(Board board, Side side, int depth, boolean maximizingPlayer) {
+		if (depth == 0 || Board.children() == null) {
+			return valueFunction(Board);
 		}
-		if(maximizingPlayer==true) {
-			int value=-1000000000;
-			State[] children = state.children();
-			for(int i=0;i<children.length();i++) {
-				value=max(value,minimax(children[i],depth-1,false));
+		if (maximizingPlayer == true) {
+			int value = -1000000000;
+			Board[] children = board.children();
+			for (int i = 0; i < children.length(); i++) {
+				value = max(value, minimax(children[i], depth - 1, false));
 			}
 			return value;
-		}else{
-			int value=+1000000000;
-			State[] children = state.children();
-			for(int i=0;i<children.length();i++) {
-				value=min(value,minimax(children[i],depth-1,false));
+		} else {
+			int value = +1000000000;
+			Board[] children = Board.children();
+			for (int i = 0; i < children.length(); i++) {
+				value = min(value, minimax(children[i], depth - 1, false));
 			}
 			return value;
 		}
-	 }
+	}
+
+	private Board[] getchildren(Board board, Side side) {
+		ArrayList<Board> children = new ArrayList<StrBoarding>();
+		for (int i = 0; i < board.getNoOfHoles() / 2; i++) {
+			if (board.getSeeds(side, i) > 0) {
+				Move newmove = new Move(side, i);
+				Kalah kalah = new Kalah(board);
+			}
+		}
+	}
 }
 
-//The assumptions are:
-//We have the 'state' passed in from the API.
-//We have the function called 'children' which returns all the states
-//that can be accessed from this chosen state.
+// The assumptions are:
+// We have the 'Board' passed in from the API.
+// We have the function called 'children' which returns all the Boards
+// that can be accessed from this chosen Board.
