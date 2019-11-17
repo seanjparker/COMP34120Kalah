b3ec406db56cd15e9ef53a2ebfc824e352f31b57
Elis Elliott <elis.elliott@student.manchester.ac.uk>
2019-11-17 13:28:28 +0000

    Add basic value function

--- MiniMax.java
+++ MiniMax.java
@@ -33,6 +33,20 @@ public class MiniMax {
 			}
 		}
 	}
+
+	// TODO(whoever): Write a better value function.
+	private double valueFunction(final Board board, final boolean maximizingPlayer) {
+		final int player = maximizingPlayer ? 0 : 1;
+		final int opponent = !maximizingPlayer ? 1 : 0;
+		int numStonesOurPits = 0;
+		int numStonesOpponentPits = 0;
+		for (int i = 0; i < board[0].length; i++) {
+			numStonesOurPits += board[player][i];
+			numStonesOpponentPits += board[opponent][i];
+		}
+
+		return (board.board[player][0] - board.board[opponent][0]) + (numStonesOurPits - numStonesOpponentPits);
+	}
 }
 
 // The assumptions are:
