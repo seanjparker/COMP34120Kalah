956a998c6ac6da6575c3790bab3e512df2c7422f
Sara Caballero Bruno <sara2cb@gmail.com>
2019-11-23 18:49:49 +0000

    Added a test for MiniMax

--- MKAgent/MiniMax.java
+++ MKAgent/MiniMax.java
@@ -1,29 +1,32 @@
+package MKAgent;
 import java.util.ArrayList;
 
-import MKAgent.Board;
+
 
 public class MiniMax {
-	public ValueObj minimax(Board board, Side side, int depth, boolean maximizingPlayer) {
-		if (depth == 0 || Board.children() == null) {
-			return valueFunction(Board);
+	
+	public static ValueObj minimax(Board board, Side side, int depth, boolean maximizingPlayer) {
+		if (depth == 0 || getChildren(board,side) == null) {
+			ValueObj value= new ValueObj();
+			value.setValue(valueFunction(board, maximizingPlayer));
+			return value;
 		}
 		if (maximizingPlayer == true) {
 			ValueObj value= new ValueObj();
 			value.setValue(-100000000);
-			Board[] children = board.children();
-			int max;
-			int newMax;
-			int move;
+			double max = -1000000;
+			double newMax;
+			int move = 0;
 			ArrayList<Board> children = getChildren(board,side);
-			for (int i = 0; i < children.length; i++) {
+			for (int i = 0; i < children.size(); i++) {
 				if(children.get(i)!=null){
-					newMax=max(value.getValue, minimax(children.get(i), depth - 1, false));
+					newMax=Math.max(value.getValue(), minimax(children.get(i), side, depth - 1, false).getValue());
 					if(newMax>max){
 						max=newMax;
 						move=i;
+						value.setValue(newMax);
+						value.setMove(move);
 					}
-					value.setValue(newMax);
-					value.setMove(move);
 				}
 			}
 			return value;
@@ -31,19 +34,18 @@ public class MiniMax {
 			ValueObj value= new ValueObj();
 			value.setValue(100000000);
 			ArrayList<Board> children = getChildren(board,side);
-			int min;
-			int newMin;
+			double min = 10000000;
+			double newMin;
 			int move;
-			Board[] children = Board.children();
-			for (int i = 0; i < children.length; i++) {
+			for (int i = 0; i < children.size(); i++) {
 				if(children.get(i)!=null){
-					newMin=min(value.getValue, minimax(children.get(i), depth - 1, true));
+					newMin=Math.min(value.getValue(), minimax(children.get(i), side, depth - 1, true).getValue());
 					if(newMin<min){
 						min=newMin;
 						move=i;
+						value.setValue(newMin);
+						value.setMove(move);
 					}
-					value.setValue(newMin);
-					value.setMove(move);
 				}
 			}
 			return value;
@@ -54,9 +56,7 @@ public class MiniMax {
 	  ArrayList<Board> children = new ArrayList<Board>();
 	  Board originalBoard = new Board(board);
 
-	  System.out.println(board.getNoOfHoles());
 	  for (int i = 1; i <= board.getNoOfHoles(); i++) {
-	    System.out.println(i);
 	    if (board.getSeeds(side, i) > 0) {
 	      Move newmove = new Move(side, i);
 	      Kalah.makeMove(board,newmove);
@@ -67,13 +67,12 @@ public class MiniMax {
 				children.add(null);
 			}
 	  }
-	  System.out.println(children);
 	  return children;
-	  }
 	}
+	
 
 	// TODO(whoever): Write a better value function.
-	private double valueFunction(final Board board, final boolean maximizingPlayer) {
+	private static double valueFunction(final Board board, final boolean maximizingPlayer) {
 		final int player = maximizingPlayer ? 0 : 1;
 		final int opponent = !maximizingPlayer ? 1 : 0;
 		int numStonesOurPits = 0;
@@ -83,7 +82,7 @@ public class MiniMax {
 			numStonesOpponentPits += board.getBoard()[opponent][i];
 		}
 
-		return (board.getBoard[player][0] - board.getBoard[opponent][0]) + (numStonesOurPits - numStonesOpponentPits);
+		return (board.getBoard()[player][0] - board.getBoard()[opponent][0]) + (numStonesOurPits - numStonesOpponentPits);
 	}
 }
 
--- MKAgent/ValueObj.java
+++ MKAgent/ValueObj.java
@@ -1,17 +1,16 @@
 package MKAgent;
 public class ValueObj{
-  private int value;
+  private double value;
   private int move;
-  valueObj(){
-
-  }
-  public int getValue(){
+  
+  
+  public double getValue(){
     return value;
   }
   public int getMove(){
     return move;
   }
-  public void setValue(int value){
+  public void setValue(double value){
     this.value=value;
   }
   public void setMove(int value){
