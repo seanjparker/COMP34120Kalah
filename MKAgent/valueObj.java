eaaeb542bc927ba513fde94d8d7de66c6c07c934
e43242pe <patrick.evans-4@student.manchester.ac.uk>
2019-11-23 17:16:17 +0000

    Add value object class

---
+++ MKAgent/valueObj.java
@@ -0,0 +1,19 @@
+public class valueObj{
+  private int value;
+  private int move;
+  valueObj(){
+
+  }
+  public getValue(){
+    return value;
+  }
+  public getMove(){
+    return move;
+  }
+  public setValue(int value){
+    this.value=value;
+  }
+  public setMove(int value){
+    this.value=value;
+  }
+}
