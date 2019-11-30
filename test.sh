30972073b19feaf03270d0d335d3383502890552
Elis Elliott <elis.elliott@student.manchester.ac.uk>
2019-11-30 16:52:34 +0000

    Print opponent name before a game in test

--- test.sh
+++ test.sh
@@ -6,10 +6,12 @@ RESULTS=results.txt
 north()
 {
     echo "MKREF" >> $RESULTS
+    echo "MKREF"
     java -jar ManKalah.jar "java -jar MKRefAgent.jar" "java -jar Agent.jar" >> $RESULTS
 
     for file in ./Agents/error404.jar; do 
         echo "${file##*/}"  >> $RESULTS
+        echo "${file##*/}"
         java -jar ManKalah.jar "java -jar $file" "java -jar Agent.jar" >> $RESULTS
     done
 }
@@ -18,10 +20,12 @@ north()
 south()
 {
     echo "MKREF" >> $RESULTS
+    echo "MKREF"
     java -jar ManKalah.jar "java -jar Agent.jar" "java -jar MKRefAgent.jar" >> $RESULTS
 
     for file in ./Agents/error404.jar; do 
         echo "${file##*/}"  >> $RESULTS
+        echo "${file##*/}"
         java -jar ManKalah.jar "java -jar Agent.jar" "java -jar $file" >> $RESULTS
     done
 }
@@ -29,14 +33,19 @@ south()
 some()
 {
     echo "MKREF" >> $RESULTS
+    echo "MKREF"
     java -jar ManKalah.jar "java -jar MKRefAgent.jar" "java -jar Agent.jar" >> $RESULTS
     echo "JIMMY" >> $RESULTS
+    echo "JIMMY"
     java -jar ManKalah.jar "java -jar Agents/JimmyPlayer.jar" "java -jar Agent.jar" >> $RESULTS
     echo "ERROR404" >> $RESULTS
+    echo "ERROR404"
     java -jar ManKalah.jar "java -jar Agents/error404.jar" "java -jar Agent.jar" >> $RESULTS
     echo "GROUP2" >> $RESULTS
+    echo "GROUP2"
     java -jar ManKalah.jar "java -jar Agents/Group2Agent.jar" "java -jar Agent.jar" >> $RESULTS
     echo "GROUP1" >> $RESULTS
+    echo "GROUP1"
     java -jar ManKalah.jar "java -jar Agents/Group1.jar" "java -jar Agent.jar" >> $RESULTS
 }
 
@@ -47,7 +56,7 @@ case "$1" in
     -a|--all)
         north
         ;;
-    -s!--south)
+    -s|--south)
         south
         ;;
     *)
