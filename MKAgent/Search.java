1929074f9f8ed9cd92fbcb92fd19ee34beca9fbe
Sean Parker <sean.parker067@gmail.com>
2019-11-30 15:28:30 +0000

    Switch to negamax Alter heuristic weights to improve performance Set depth to 9

--- MKAgent/Search.java
+++ MKAgent/Search.java
@@ -1,6 +1,8 @@
 package MKAgent;
 
+import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.lang.Math;
 
 public class Search {
@@ -10,87 +12,43 @@ public class Search {
 	}
 
 	private static ValueObj alphaBeta(Board board, Side side, int depth, double alpha, double beta, boolean maximizingPlayer) {
-		if (depth == 0 || getChildren(board, side) == null) {
-			ValueObj value = new ValueObj();
+		ValueObj value = new ValueObj();
+		if (depth == 0 || getSortedChildren(board, side, maximizingPlayer).size() == 0) {
 			value.setValue(Evaluation.evaluate(board, side, maximizingPlayer));
 			return value;
 		}
-		if (maximizingPlayer) {
-			ValueObj value = new ValueObj();
-			value.setValue(Integer.MIN_VALUE);
-			int max = Integer.MIN_VALUE;
-			int newMax;
-			int move = 0;
-			ArrayList<Board> children = getChildren(board, side);
+		List<ValueObj> children = getSortedChildren(board, side, maximizingPlayer);
+		ValueObj best = new ValueObj();
+		best.setValue(Integer.MIN_VALUE);
 
-			for (int i = 0; i < children.size(); i++) {
+		for (int i = 0; i < children.size(); i++) {
+			Board nextBoard = new Board(board);
+			Kalah.makeMove(nextBoard, new Move(side, children.get(i).getMove()));
+			value = alphaBeta(nextBoard, side.opposite(), depth - 1, -beta, -alpha, !maximizingPlayer);
+			value.setValue(-value.getValue());
 
-				if (children.get(i) != null) {
-
-					newMax = alphaBeta(children.get(i), side.opposite(), depth - 1, alpha, beta, false).getValue();
-					alpha = Math.max(alpha, value.getValue());
-					if (alpha >= beta) {
-						break;
-					}
-					if (newMax > max) {
-						max = newMax;
-						move = i;
-						value.setValue(newMax);
-						value.setMove(move + 1);
-					}
-				}
-
+			// Value is a better move than best
+			if (value.compareTo(best) > 0) {
+				value.setMove(children.get(i).getMove());
+				best = value.clone();
 			}
-
-			return value;
-		} else {
-			ValueObj value = new ValueObj();
-			value.setValue(Integer.MAX_VALUE);
-
-			ArrayList<Board> children = getChildren(board, side);
-
-			int min = Integer.MAX_VALUE;
-			int newMin;
-			int move;
-			for (int i = 0; i < children.size(); i++) {
-
-				if (children.get(i) != null) {
-
-					newMin = alphaBeta(children.get(i), side.opposite(), depth - 1, alpha, beta, true).getValue();
-					beta = Math.min(beta, value.getValue());
-					if (alpha >= beta) {
-						break;
-					}
-					if (newMin < min) {
-						min = newMin;
-						move = i;
-						value.setValue(newMin);
-						value.setMove(move + 1);
-					}
-				}
-			}
-
-			return value;
+			if (best.getValue() > alpha)
+				alpha = best.getValue();
+			if (alpha >= beta)
+				break; // Cut off the current branch
 		}
+
+		return best;
 	}
 
-	public static ArrayList<Board> getChildren(Board board, Side side) {
-		ArrayList<Board> children = new ArrayList<Board>();
-		Board originalBoard = new Board(board);
-		boolean hasChildren = false;
-		for (int i = 1; i <= originalBoard.getNoOfHoles(); i++) {
-			if (Kalah.isLegalMove(originalBoard, new Move(side, i))) {
-				Move newmove = new Move(side, i);
-				Kalah.makeMove(originalBoard, newmove);
-				children.add(originalBoard);
-				originalBoard = new Board(board);
-				hasChildren = true;
-			} else {
-				children.add(null);
+	public static List<ValueObj> getSortedChildren(Board board, Side side, boolean maximizingPlayer) {
+		List<ValueObj> children = new ArrayList<ValueObj>();
+		for (int i = 1; i <= board.getNoOfHoles(); i++) {
+			if (board.getSeeds(side, i) != 0) {
+				children.add(new ValueObj(i, Evaluation.evaluate(board, side, maximizingPlayer)));
 			}
 		}
-		if (!hasChildren)
-			children = null;
+		Collections.sort(children);
 		return children;
 	}
 }
