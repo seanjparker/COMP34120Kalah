f3f26ce28210e72eee435ad68ec874e7b0760ab7
Elis Elliott <elis.elliott@student.manchester.ac.uk>
2019-11-30 16:41:32 +0000

    Add option in test to play all agents

--- test.sh
+++ test.sh
@@ -2,18 +2,42 @@
 
 RESULTS=results.txt
 
+all()
+{
+    echo "MKREF" >> $RESULTS
+    java -jar ManKalah.jar "java -jar MKRefAgent.jar" "java -jar Agent.jar" >> $RESULTS
+
+    for file in ./Agents/error404.jar; do 
+        echo "${file##*/}"  >> $RESULTS
+        java -jar ManKalah.jar "java -jar $file" "java -jar Agent.jar" >> $RESULTS
+    done
+}
+
+some()
+{
+    echo "MKREF" >> $RESULTS
+    java -jar ManKalah.jar "java -jar MKRefAgent.jar" "java -jar Agent.jar" >> $RESULTS
+    echo "JIMMY" >> $RESULTS
+    java -jar ManKalah.jar "java -jar Agents/JimmyPlayer.jar" "java -jar Agent.jar" >> $RESULTS
+    echo "ERROR404" >> $RESULTS
+    java -jar ManKalah.jar "java -jar Agents/error404.jar" "java -jar Agent.jar" >> $RESULTS
+    echo "GROUP2" >> $RESULTS
+    java -jar ManKalah.jar "java -jar Agents/Group2Agent.jar" "java -jar Agent.jar" >> $RESULTS
+    echo "GROUP1" >> $RESULTS
+    java -jar ManKalah.jar "java -jar Agents/Group1.jar" "java -jar Agent.jar" >> $RESULTS
+}
+
+
 echo "START" > $RESULTS
 
-echo "MKREF" >> $RESULTS
-java -jar ManKalah.jar "java -jar MKRefAgent.jar" "java -jar Agent.jar" >> $RESULTS
-echo "JIMMY" >> $RESULTS
-java -jar ManKalah.jar "java -jar Agents/JimmyPlayer.jar" "java -jar Agent.jar" >> $RESULTS
-echo "ERROR404" >> $RESULTS
-java -jar ManKalah.jar "java -jar Agents/error404.jar" "java -jar Agent.jar" >> $RESULTS
-echo "GROUP2" >> $RESULTS
-java -jar ManKalah.jar "java -jar Agents/Group2Agent.jar" "java -jar Agent.jar" >> $RESULTS
-# echo "GROUP1" >> $RESULTS
-# java -jar ManKalah.jar "java -jar Agents/Group1.jar" "java -jar Agent.jar" >> $RESULTS
+case "$1" in
+    -a|--all)
+        all
+        ;;
+    *)
+        some
+        ;;
+esac
 
 echo "END" >> $RESULTS
 
